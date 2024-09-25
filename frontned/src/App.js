f26988969ec115dca22064b005f85a8e143fcbc9
A3zooz <hmoda201@gmail.com>
2024-09-25 13:56:11 +0300

    fixing

---
+++ frontned/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import TutorManagement from './components/TutorManagement';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Tutor Management System</h1>
+      </header>
+      <main>
+        <TutorManagement />
+      </main>
+    </div>
+  );
+}
+
+export default App;
